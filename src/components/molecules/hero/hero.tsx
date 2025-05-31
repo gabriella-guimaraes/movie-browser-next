bc80c2304547a130016d2b990b54b0addeedc7b9
gabriella-guimaraes <gabigara13@gmail.com>
2025-05-31 18:17:50 -0300

    styles: hero titles animations

--- src/components/molecules/hero/hero.tsx
+++ src/components/molecules/hero/hero.tsx
@@ -3,8 +3,8 @@ import styles from "./hero.module.css";
 export default function Hero(){
     return(
         <section className={styles.Hero}>
-            <h1 className="BannerTitle">Movie Browser</h1>
-            <p className="SubTitle">Track films you’ve watched. <br/>
+            <h1 className="BannerTitle animeLeft">Movie Browser</h1>
+            <p className="SubTitle text-focus-in">Track films you’ve watched. <br/>
                 Tell your friends what’s good.</p>
         </section>
     )
